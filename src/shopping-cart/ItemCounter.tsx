aeeec5c15c1f7bef5b92b4c3be91f68bf0574694
david sanchez <sanchezcallizo@gmail.com>
2025-12-14 12:12:36 +0100

    seccion 4 acabada

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,5 +1,10 @@
 //rafc
-import React from 'react'
+import { useState } from 'react'
+
+
+// import './ItemCounter.css'
+import style from './ItemCounter.module.css'
+
 
 interface Props {
     name: string;
@@ -7,20 +12,46 @@ interface Props {
 }
 
 export const ItemCounter = ({ name, quantity }: Props) => {
+    const [count, setCount] = useState(quantity);
+
+    const handleAddClick = () => {
+        setCount(count + 1);
+        console.log(`Añado uno en ${name}, quantity: ${quantity}`);
+
+    }
+
+    const handleSubtractClick = () => {
+        setCount(count - 1);
+        console.log(`Quito uno en ${name}, quantity: ${quantity}`);
+    }
+
+
     return (
-        <section style={
-            {
-                display: 'flex',
-                alignItems: 'center',
-                gap: '1rem',
-                marginTop: '10px',
-            }
-        }>
-            <p>{name}</p>
-            <button>+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
-        </section>
+        <section
+            //className="item-row"
+            className={style.itemRow}
+        // style={
+        //     {
+        //         display: 'flex',
+        //         alignItems: 'center',
+        //         gap: '1rem',
+        //         marginTop: '10px',
+        //     }}
+        >
+            <p
+                className={style.itemText}
+                style = {
+                    {
+                        color: count === 0 ? 'red' : 'black'
+                    }
+                }
+                //className={count === 0 ? style.red : style.black}
+                
+            >{name}</p>
+            <button onClick={handleAddClick}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtractClick}>-1</button>
+        </section >
     )
 }
 
